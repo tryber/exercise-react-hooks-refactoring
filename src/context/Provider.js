cf7cdc7dc3d3f8d45d7f6939a942d8753322df42
Lais Gurgel <laisgurgelf@gmail.com>
2020-11-13 15:21:11 -0300

    18_2 - Exercise 1 - Refactoring with React Hooks - useState and useContext

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,49 +1,31 @@
-// src/context/Provider.js
-
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
+function Provider({ children }) {
+  const carsDefault = {
+    red: false,
+    blue: false,
+    yellow: false,
   }
+  const [cars, setState ] = useState(carsDefault);
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
-    });
+  const moveCar = (car, side) => {
+    setState({
+      ...cars,
+      [car]: side,
+    })
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
+  const context = {
+    cars,
+    moveCar,
+  };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
-};
-
-Provider.propTypes = {
-  children: PropTypes.node.isRequired,
+  return (
+    <CarsContext.Provider value={context}>
+      {children}
+    </CarsContext.Provider>
+  );
 };
 
 export default Provider;
